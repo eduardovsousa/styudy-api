b4daeb4b0969445a9265de4d036dbfae29421752
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 22:42:26 -0300

    Configurando o json server

---
+++ src/main/java/com/github/eduardovsousa/study_api/controller/PessoaController.java
@@ -0,0 +1,22 @@
+package com.github.eduardovsousa.study_api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.eduardovsousa.study_api.model.Pessoa;
+import com.github.eduardovsousa.study_api.service.PessoaService;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+	
+	private PessoaService pessoaService = new PessoaService();
+	
+	@GetMapping
+	public List<Pessoa> listAll() {
+		return pessoaService.list();
+	}
+}
---
+++ src/main/java/com/github/eduardovsousa/study_api/service/PessoaService.java
@@ -0,0 +1,40 @@
+package com.github.eduardovsousa.study_api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.eduardovsousa.study_api.model.Pessoa;
+
+public class PessoaService {
+	private List<Pessoa> pessoas;
+	
+	public PessoaService() {
+		load();
+	}
+ 	
+	private void load() {
+		pessoas = new ArrayList<>();
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setId(1l);
+		pessoa.setNome("Eduardo");
+		pessoa.setIdade(26);
+		pessoas.add(pessoa);
+		
+		pessoa = new Pessoa();
+		pessoa.setId(2l);
+		pessoa.setNome("Ellen");
+		pessoa.setIdade(25);
+		pessoas.add(pessoa);
+
+		pessoa = new Pessoa();
+		pessoa.setId(3l);
+		pessoa.setNome("Theo");
+		pessoa.setIdade(6);
+		pessoas.add(pessoa);
+	}
+	
+	public List<Pessoa> list() {
+		return pessoas;
+	}
+}
